177ec13ed9f4e524a67594a065710a6e761d114d
duynn22 <duynn22@viettel.com.vn>
2024-01-12 17:12:48 +0700

    [CDN-98] feature: fix git rebase

--- src/test/java/com/viettel/vtnet/addtokenservice/AddTokenServiceApplicationTests.java
+++ src/test/java/com/viettel/vtnet/addtokenservice/AddTokenServiceApplicationTests.java
@@ -6,20 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AddTokenServiceApplicationTests {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  @Test
-  void contextLoads() {
-  }
-=======
-	@Test
-	void contextLoads() {
-	}
->>>>>>> a7d5f61 (Initial commit)
-=======
-  @Test
-  void contextLoads() {
-  }
->>>>>>> 6764d70 (save)
-
 }
